6a994d867dd0ad376f4fc3632b914df411fac403
Drew Perttula <drewp@bigasterisk.com>
2018-11-26 01:53:04 -0800

    cleanups. monitor baudrate setting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,3 @@
-/* Blink Example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -12,10 +6,10 @@
 
 #include "Adafruit_BME280.h"
 #include "Adafruit_CCS811.h"
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
 #include "PMS.h"
+#include <Adafruit_Sensor.h>
+#include <SPI.h>
+#include <Wire.h>
 
 Adafruit_CCS811 ccs;
 
